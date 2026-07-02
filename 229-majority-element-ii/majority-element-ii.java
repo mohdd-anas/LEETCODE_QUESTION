a3b59f50059d516fc4c1fe304cdd86810d85508a
mohdd-anas <mohdanas48925@gmail.com>
2026-07-02 19:00:30 +0530

    Time: 3 ms (72.80%) | Memory: 52.7 MB (40.12%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,17 +1,56 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        ArrayList<Integer> arr=new ArrayList<>();
+        int count1=0;
+        int count2=0;
+        int element1=0;
+        int element2=0;
         for(int i=0;i<nums.length;i++)
         {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int j:map.keySet())
-        {
-            if(map.get(j)>nums.length/3)
+            if(count1==0 &&nums[i]!=element2)
             {
-                arr.add(j);
+                count1++;
+                element1=nums[i];
+
             }
+            else if(count2==0 && nums[i]!=element1)
+            {
+                count2++;
+                element2=nums[i];
+
+            }
+            else if(nums[i]==element1)
+            {
+                count1++;
+            }
+            else if(nums[i]==element2)
+            {
+                count2++;
+            }
+            else 
+            {
+                count1--;
+            
+            
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == element1) {
+                count1++;
+            } else if (num == element2) {
+                count2++;
+            }
+        }
+        ArrayList<Integer> arr=new ArrayList<>();
+        if (count1 > nums.length / 3)
+        {
+            arr.add(element1);
+        }
+        if (count2 > nums.length / 3)
+        {
+            arr.add(element2);
         }
         return arr;
     }
