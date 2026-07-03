699f88c0309a4289e07cc24e1119a1c7f19d069b
mohdd-anas <mohdanas48925@gmail.com>
2026-07-03 09:52:05 +0530

    Time: 20 ms (64.77%) | Memory: 46.1 MB (16.34%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,25 +1,61 @@
-class Solution {
-    public List<List<Integer>> fourSum(int[] arr, int target) {
+class Solution 
+{
+    public List<List<Integer>> fourSum(int[] arr, int target) 
+    {
+
         HashSet<List<Integer>> ans=new HashSet<>();
+        Arrays.sort(arr);
         for(int i=0;i<arr.length;i++)
         {
-            for(int j=i+1;j<arr.length;j++)
+            if(i>0 && arr[i]==arr[i-1])
             {
-                HashSet<Long> store=new HashSet<>();
-                for(int k=j+1;k<arr.length;k++)
-                {
-                   long sum=(long)arr[i]+arr[j]+arr[k];
-                   long need=(long)target-sum;
-                    if(store.contains(need))
-                    {
-                        List<Integer> temp=Arrays.asList(arr[i],arr[j],arr[k],(int)need);
-                        Collections.sort(temp);
-                        ans.add(temp);
-                    }
-                    store.add((long)arr[k]);
-                }
+                continue;
             }
+        
+           for(int j=i+1;j<arr.length;j++)
+           {
+            if(j>i+1 && arr[j]==arr[j-1])
+            {
+                continue;
+            }
+        
+            // int j=i+1;
+            int k=j+1;
+            int l=arr.length-1;
+            while(k<l)
+            {
+                long sum=(long)arr[i]+arr[j]+arr[k]+arr[l];
+                if(sum>target)
+                {
+                    l--;
+
+                }
+                else if(sum<target)
+                {
+                    k++;
+                }
+                else
+                {
+                    List<Integer> temp=Arrays.asList(arr[i],arr[j],arr[k],arr[l]);
+                    ans.add(temp);
+                    k++;
+                    l--;
+                    while(k<l && arr[k]==arr[k-1])
+                {
+                    k++;
+                }
+                while(k<l && arr[l]==arr[l+1])
+                {
+                     l--;
+                }
+                }
+                
+                
+            }
+           }
         }
-        return new ArrayList<>(ans);
+         return new ArrayList<>(ans);
     }
 }
+        
+
