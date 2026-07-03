78fa496bc25c52203a1bf71ed81989fc93b6b8b0
mohdd-anas <mohdanas48925@gmail.com>
2026-07-04 01:12:32 +0530

    Time: 12 ms (9.70%) | Memory: 48.6 MB (97.53%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,42 +1,25 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-        List<List<Integer>> list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++)
-        {
-            int start=arr[i][0];
-            int end=arr[i][1];
+    public int[][] merge(int[][] intervals) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < intervals.length; i++) {
+            if (ans.isEmpty() || ans.get(ans.size() - 1).get(1) < intervals[i][0]) {
 
-            if(!list.isEmpty() && end<=list.get(list.size()-1).get(1))
-            {
-                continue;
+                ans.add(new ArrayList<>(Arrays.asList(intervals[i][0], intervals[i][1])));
+            } else {
+                int end = Math.max(
+                        ans.get(ans.size() - 1).get(1),
+                        intervals[i][1]);
+                ans.get(ans.size() - 1).set(1, end);
             }
-            int j;
-            for(j=i+1;j<arr.length;j++)
-            {
-                if(end>=arr[j][0])
-                {
-                    end=Math.max(arr[j][1],end);
-                }
-                else
-                {
-                    break;
-                }
-
-            }
-            list.add(Arrays.asList(start,end));
-            i=j-1;
-
         }
-        
-        int[][] ans = new int[list.size()][2];
+        int[][] result = new int[ans.size()][2];
 
-for (int i = 0; i < list.size(); i++) {
-    ans[i][0] = list.get(i).get(0);
-    ans[i][1] = list.get(i).get(1);
-}
+        for (int i = 0; i < ans.size(); i++) {
+            result[i][0] = ans.get(i).get(0);
+            result[i][1] = ans.get(i).get(1);
+        }
 
-return ans;
-        
+        return result;
     }
 }
