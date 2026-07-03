df43bbee2141822dd66453a77e4153fe500e6aa9
mohdd-anas <mohdanas48925@gmail.com>
2026-07-04 03:24:50 +0530

    Time: 14 ms (15.19%) | Memory: 46.9 MB (64.63%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int i:map.keySet())
+        {
+            if(map.get(i)==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
