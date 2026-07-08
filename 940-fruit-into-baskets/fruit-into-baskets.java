846bc9b87377dd0e9ec769fb1189d59691a3baa9
mohdd-anas <mohdanas48925@gmail.com>
2026-07-09 01:17:45 +0530

    Time: 54 ms (66.91%) | Memory: 70.8 MB (17.96%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int i=0;
+        int j=0;
+        int max=0;
+        int len=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(j<fruits.length)
+        {
+            map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+            while(map.size()>2)
+            {
+                map.put(fruits[i],map.getOrDefault(fruits[i],0)-1);
+                if(map.get(fruits[i])==0)
+                {
+                    map.remove(fruits[i]);
+                }
+                i++;
+            }
+            len=j-i+1;
+            max=Math.max(len,max);
+             
+            j++;
+        }
+        return max;
+    }
+}
