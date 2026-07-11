115162dc00f2f8315d8bc34d47951fa280d03158
mohdd-anas <mohdanas48925@gmail.com>
2026-07-11 17:01:47 +0530

    Time: 1 ms (99.99%) | Memory: 50.7 MB (83.67%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,28 +1,35 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-
-        int a = countSubArray(nums, goal);
-        int b = countSubArray(nums, goal - 1);
-        return a - b;
+        return countSubarray(nums,goal)-countSubarray(nums,goal-1);
     }
-
-    public int countSubArray(int[] arr, int goal) {
-       int i=0;
-       int j=0;
-       int sum=0;
-       int count=0;
-       while(j<arr.length)
-       {
-        if(goal<0) return 0;
-        sum=sum+arr[j];
-        while(sum>goal)
+    public int countSubarray(int[] arr,int goal)
+    {
+        if(goal<0)
         {
-            sum=sum-arr[i];
-            i++;
+            return 0;
         }
-        count=count+(j-i+1);
-        j++;
-       }
-       return count;
+        int i=0;
+        int j=0;
+        int count=0;
+        int sum=0;
+        while(j<arr.length)
+        {
+            
+            sum=sum+arr[j];
+            
+            while(sum>goal)
+            {
+                sum=sum-arr[i];
+                i++;
+            }
+            if(sum<=goal)
+            {
+                count=count+(j-i+1);
+            }
+            j++;
+
+        }
+        return count;
+
     }
 }
