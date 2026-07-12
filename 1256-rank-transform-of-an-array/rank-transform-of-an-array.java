e3700ca48562a00a6f5ef5f2c09ea4f908195633
mohdd-anas <mohdanas48925@gmail.com>
2026-07-13 00:10:56 +0530

    Time: 29 ms (84.49%) | Memory: 77 MB (31.99%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -2,39 +2,20 @@ class Solution {
     public int[] arrayRankTransform(int[] arr) {
         int[] clone=arr.clone();
         Arrays.sort(clone);
-        if(Arrays.equals(clone,arr))
-        {   boolean flag=true;
-            for(int z=1;z<clone.length;z++)
-            {
-                if(clone[0]!=clone[z])
-                {
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag)
-            {
-            int[] ans=new int[arr.length];
-            Arrays.fill(ans,1);
-            return ans;
-            }
-
-        }
         HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(clone[0],1);
-        for(int i=1;i<clone.length;i++)
+        int rank=1;
+        for(int i=0;i<clone.length;i++)
         {
             if(!map.containsKey(clone[i]))
             {
-            map.put(clone[i],map.get(clone[i-1])+1);
+                map.put(clone[i],rank++);
             }
         }
-        int[] ans=new int[arr.length];
         for(int j=0;j<arr.length;j++)
         {
             arr[j]=map.get(arr[j]);
         }
         return arr;
-        
     }
+
 }
