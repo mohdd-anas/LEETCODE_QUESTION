7e27667c12bc9668314e2140dbe8d2d278086623
mohdd-anas <mohdanas48925@gmail.com>
2026-08-03 00:56:10 +0530

    Time: 49 ms (51.38%) | Memory: 47.1 MB (63.94%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
+        }
+        if(sum%2!=0)
+        {
+            return false;
+        }
+        else
+        {
+            return solve(sum/2,nums,nums.length);
+        }
+    }
+    public static boolean solve(int sum,int[] nums,int len)
+    {
+        boolean[][] dp=new boolean[nums.length+1][sum+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            dp[i][0]=true;
+        }
+        for(int i=1;i<dp.length;i++)
+        {
+            for(int j=1;j<dp[i].length;j++)
+            {
+                if(nums[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.length][sum];
+    }
+
+}
